a22129ea0e61c45b05cc04c71891168580a7ea08
Chris Palmer <cgpalmer91@gmail.com>
2021-02-03 22:55:53 +0000

    A new eay of doing a pwa

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
-  
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('service worker registered'))
-      .catch(err => console.log('service worker not registered', err));
-  }
+navigator.serviceWorker &&
+navigator.serviceWorker.register('SW.js').then(function (registration)
+{
+});
--- sw.js
+++ sw.js
@@ -1,37 +1,3 @@
-const staticCacheName = 'thebookclub-static';
-const assets = [
-  '/',
-  '/index.html',
-  '/contactus.html',
-  '/assets/css/style.css',
-  '/assets/js/main.js'
-];
-// install event
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
-});
-// activate event
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-// When we change the name we could have multiple cache, to avoid that we need to delet the old cache, so with this function we check the key that is our cache naming, if it is different from the actual naming we delete it, in this way we will always have only the last updated cache.
-// fetch event
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+/** An empty service worker! */
+self.addEventListener('fetch', function(event) {
 });
